f7f940570bbc3741e113cbfa3d5f0af1ca9c830f
Adi Sk <skadimoolam@gmail.com>
2020-07-12 17:11:35 +0530

    Tiny fix

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,7 +5,7 @@
 
   <main class="max-w-5xl mx-auto px-4 text-center">
     <h1 class="text-3xl mt-10">{{ config('app.name') }}</h1>
-    <p class="mt-4">Lauching Soon...</p>
+    <p class="mt-4">Launching Soon...</p>
   </main>
 
 @endsection
