46331815d23495d4caa3a681035774e2b4fd351a
Adi Sk <skadimoolam@gmail.com>
2020-08-02 18:31:32 +0530

    Tiny updates to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    protected $fillable = ['name', 'email', 'password'];
+    use SoftDeletes;
+
+    protected $date = ['created_at', 'updated_at', 'deleted_at'];
+    protected $fillable = ['name', 'email', 'password', 'role'];
     protected $hidden = ['password', 'remember_token'];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('role')->default('admin');
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
